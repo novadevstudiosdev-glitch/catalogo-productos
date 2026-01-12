5aa40ff360789fbc3fe9f988afddd99161294ba9
Valentin <valentinhernandezmateo@gmail.com>
2026-01-12 17:31:03 -0300

    primer commit: tecnologias principales

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="p-6">
+      <h1 className="text-3xl font-bold text-red-600">Camisetas de fútbol personalizadas</h1>
+    </main>
+  );
+}
